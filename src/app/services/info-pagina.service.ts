8fbdf478e9335e962a093e9b24ae1aee4bccb4e8
Alejandro Campero <campero9813@gmail.com>
2020-01-28 17:02:33 -0600

    Fin de la seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,20 +10,33 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient) {
 
-// console.log('servicio de infopagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
-// Leer el archivo JSON
-this.http.get('assets/data/data-pagina.json')
+
+  private cargarInfo() {
+    // Leer el archivo JSON
+    this.http.get('assets/data/data-pagina.json')
     .subscribe( (resp: InfoPagina) => {
 
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
     });
 
   }
 
+  private cargarEquipo() {
+    this.http.get('https://angular-html-6f918.firebaseio.com/equipo.json')
+    .subscribe( (resp: any) => {
+      this.equipo = resp;
+      // console.log(resp);
+    });
+    
+  }
 
 }
